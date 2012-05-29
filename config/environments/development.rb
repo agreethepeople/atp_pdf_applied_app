65b4e3ec1bb9d0a0fe79d42f0c083859be88854a
Vanessa Hurst <vrhurst@gmail.com>
2012-05-29 12:54:22 -0400

    add google analytics tracking (per environment)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Platform::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Analytics
+  GOOGLE_ANALYTICS_TRACKING_CODE = "UA-32192787-3"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,7 @@ Platform::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Analytics
-  GOOGLE_ANALYTICS_TRACKING_CODE = "UA-32192787-1"
+  GOOGLE_ANALYTICS_TRACKING_CODE = "UA-32192787-2"
 
   # Code is not reloaded between requests
   config.cache_classes = true
