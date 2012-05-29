9ffa4eeb17c0b4aebf800eaef9c68fe8504c769f
Vanessa Hurst <vrhurst@gmail.com>
2012-05-29 11:01:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Agree The People</h1>
+The online platform that changes how we discuss and understand important issues.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Platform::Application.config.secret_token = 'a2d70ba51b5c8f31b8312c374f871b251332712f0bebc63e6bc55806e157bf22523d7620e3ffa032841659609172a3f75c022d0861c944d7b7517e58fbcbd76b'
