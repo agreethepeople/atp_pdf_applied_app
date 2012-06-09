0fe93d1bd1e193555e82ad16966b852f3cd71874
Vanessa Hurst <vrhurst@gmail.com>
2012-06-09 17:53:30 -0400

    replace missing comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable, :confirmable
         #:omniauthable
 
